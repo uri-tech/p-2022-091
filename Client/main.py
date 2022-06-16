3eb3fb3af9b853cb2929e7ce879eaeb0186f3a8e
uri <uri.al.1500@gmail.com>
2022-06-17 00:09:41 +0300

    thread fix 3

--- Client/main.py
+++ Client/main.py
@@ -17,12 +17,18 @@ try:
     d = {'nodeip': 'ip', 'type': 'Client-Container'}
     logger = logging.getLogger('tcpserver')
     logger.setLevel(1)
+    hostLowArea = getenv('HOST_LOW_AREA')
+    if hostLowArea is not None:
+        HOST_LOW_AREA = "localhost"
+    portLowArea = getenv('PORT_LOW_AREA')
+    if portLowArea is not None:
+        PORT_LOW_AREA = "8012"
 except Exception as ex:
     raise Exception(f"ERROR:\n{ex}")
 
-def ClientThread(client_id)):
+
+def ClientThread(client_id):
     try:
-        HOST, PORT = "localhost", 9999
         CLIENT_START_dBm: str = "4444dBm"
         while(True):
             CLIENT_MIGRATION_TIME: float = uniform(100, 130)
@@ -31,7 +37,7 @@ def ClientThread(client_id)):
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 # Connect to server and send data
                 sock.settimeout(CLIENT_MIGRATION_TIME)
-                sock.connect((HOST, PORT))
+                sock.connect((HOST_LOW_AREA, PORT_LOW_AREA))
                 start_time = time()
                 sock.sendall(bytes(CLIENT_START_dBm + CLIENT_ID_LIST[idxClientId] + temp_time + "1"*100 + "\n", "utf-8"))
                 # Receive data from the server and shut dow
@@ -54,16 +60,16 @@ if __name__ == '__main__':
         shuffle(CLIENT_ID_LIST)
         idxClientId: int = 0
         while(True):
-            idxClientId +=1
-            if(idxClientId >=NUM_OF_CLIENT):
+            idxClientId += 1
+            if(idxClientId >= NUM_OF_CLIENT):
                 idxClientId = 0
             while(threading.active_count() != NUM_OF_CLIENT):
-                x = threading.Thread(target=ClientThread, args=(CLIENT_ID_LIST[idxClientId]), daemon=True)
-                x.start()
+                client_t = threading.Thread(target=ClientThread, args=(CLIENT_ID_LIST[idxClientId],), daemon=True)
+                client_t.start()
     except Exception as ex:
         raise Exception(f"main: {ex}")
 
 # # return None if not exist
-# user_mongo = getenv('HOST_LOW_AREA')
+# hostLowArea = getenv('HOST_LOW_AREA')
 # if user_mongo is not None:
 #     HOST_LOW_AREA = str(hostLowArea)
