37af64332845f45bce80431a59fed79e93aada70
uri <uri.al.1500@gmail.com>
2022-06-16 23:48:11 +0300

    client-Satellite

--- Client/main.py
+++ Client/main.py
@@ -1,70 +1,73 @@
-# # pip install import_from_github_com
-# try:
-#     import logging
-#     import socketserver
-#     from modulos.network import CustomTCPHandler
-#     from modulos.secrets import HOST, PORT
-#     # from github_com.kennethreitz import requests
-#     # assert requests.get('https://github.com/p-2022-091/modulos/network.py').status_code == 200
+# pip install import_from_github_com
+try:
+    import logging
+    from datetime import datetime, timedelta
+    from os import getenv
+    import threading
+    # from modulos.secrets import HOST, PORT
+    import socket
+    from time import time
+    from random import uniform, randint, shuffle
+    # from github_com.kennethreitz import requests
+    # assert requests.get('https://github.com/p-2022-091/modulos/network.py').status_code == 200
 
-#     # formating the log
-#     FORMAT = '%(asctime)-15s %(nodeip)s %(type)-8s, message: %(message)s'
-#     logging.basicConfig(format=FORMAT)
-#     d = {'nodeip': 'ip', 'type': 'Satellite-Container'}
-#     logger = logging.getLogger('tcpserver')
-#     logger.setLevel(1)
-# except Exception as ex:
-#     raise Exception(f"ERROR:\n{ex}")
-
-
-# if __name__ == '__main__':
-#     try:
-#         # Create the server, binding to ip HOST on PORT 
-#         with socketserver.ThreadingTCPServer((HOST, PORT), CustomTCPHandler) as server:
-#             # server.timeout = 5
-#             # server.handle_timeout()
-#             logger.warning(f"Listening on {HOST} in port {PORT}", extra=d)
-#             # Activate the server; this will keep running until  interrupt the program with Ctrl-C
-#             server.serve_forever()
-#             # while True:
-#             #     server.handle_request()
-#     except Exception as ex:
-#         raise Exception(f"main: {ex}")
-
-import socket
-from time import time
-from random import uniform
+    # formating the log
+    FORMAT = '%(asctime)-15s %(nodeip)s %(type)-8s, message: %(message)s'
+    logging.basicConfig(format=FORMAT)
+    d = {'nodeip': 'ip', 'type': 'Client-Container'}
+    logger = logging.getLogger('tcpserver')
+    logger.setLevel(1)
+except Exception as ex:
+    raise Exception(f"ERROR:\n{ex}")
 
 
 if __name__ == '__main__':
     try:
         HOST, PORT = "localhost", 9999
-        # SATELLITE_MIGRATION_TIME: float = uniform(100, 130)
-        data = bytes("dbm"+"7777777"+"1"*10, "utf-8")
-        print(f"type: {type(data)}, __sizeof__: {(data.__sizeof__())}")
-
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        # create_connection() -- connects to an address, with an optional timeout and
-        # with socket.create_connection(socket.AF_INET, socket.SOCK_STREAM, timeout=10.0) as sock:
-
+        NUM_OF_CLIENT: int = 1000
+        CLIENT_ID_LIST: list = [i for i in range(0, 1000000+NUM_OF_CLIENT)]
+        shuffle(CLIENT_ID_LIST)
+        CLIENT_START_dBm: str = "4444dBm"
+        # clientThreadsList: list = []
+        # for _ in range(NUM_OF_CLIENT):
         while(True):
-            SATELLITE_MIGRATION_TIME: float = uniform(100, 130)
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                # Connect to server and send data
-                sock.settimeout(SATELLITE_MIGRATION_TIME)
-                # sock.setdefaulttimeout(3)
-                sock.connect((HOST, PORT))
-                start_time = time()
-                sock.sendall(bytes(data + "\n", "utf-8"))
-
-                # Receive data from the server and shut dow
-                received = str(sock.recv(1024), "utf-8")
-                sock.sendall(bytes(data + "\n", "utf-8"))
-                print(f"sock.gettimeout(): {sock.gettimeout()}, time():{time()}, start_time: {start_time}, {time()-start_time}")
-                while(sock.gettimeout() > time()-start_time):
-                    pass
-
-            print(f"Sent:     {data}")
-            print(f"Received from the server:  {received}")
+            while(threading.active_count() != NUM_OF_CLIENT):
+                x = threading.Thread(target=ClientThread, args=(), daemon=True)
+                x.start()
     except Exception as ex:
         raise Exception(f"main: {ex}")
+
+
+def ClientThread():
+    try:
+        HOST, PORT = "localhost", 9999
+        NUM_OF_CLIENT: int = 1000
+        CLIENT_ID_LIST: list = [i for i in range(0, 1000000+NUM_OF_CLIENT)]
+        shuffle(CLIENT_ID_LIST)
+        CLIENT_START_dBm: str = "4444dBm"
+        while(True):
+            CLIENT_MIGRATION_TIME: float = uniform(100, 130)
+            temp_time = int((datetime.now() + timedelta(seconds=CLIENT_MIGRATION_TIME)).strftime("%Y%m%d%H%M%S"))
+            idxClientId: int = randint(0, NUM_OF_CLIENT)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                # Connect to server and send data
+                sock.settimeout(CLIENT_MIGRATION_TIME)
+                sock.connect((HOST, PORT))
+                start_time = time()
+                sock.sendall(bytes(CLIENT_START_dBm + CLIENT_ID_LIST[idxClientId] + temp_time + "1"*100 + "\n", "utf-8"))
+                # Receive data from the server and shut dow
+                # received = str(sock.recv(1024), "utf-8")
+
+                while(sock.gettimeout() > time()-start_time):
+                    pass
+            tempIdxClientId: int = randint(0, NUM_OF_CLIENT)
+            while(tempIdxClientId == idxClientId):
+                tempIdxClientId: int = randint(0, NUM_OF_CLIENT)
+            idxClientId = tempIdxClientId
+    except Exception as ex:
+        print(f"ClientThread: {ex}")
+
+# # return None if not exist
+# user_mongo = getenv('HOST_LOW_AREA')
+# if user_mongo is not None:
+#     HOST_LOW_AREA = str(hostLowArea)
--- Satellite/main.py
+++ Satellite/main.py
@@ -1,69 +1,50 @@
-# # pip install import_from_github_com
-# try:
-#     import logging
-#     import socketserver
-#     from modulos.network import CustomTCPHandler
-#     from modulos.secrets import HOST, PORT
-#     # from github_com.kennethreitz import requests
-#     # assert requests.get('https://github.com/p-2022-091/modulos/network.py').status_code == 200
+# pip install import_from_github_com
+try:
+    import logging
+    # from modulos.secrets import HOST, PORT
+    import socket
+    from time import time
+    from random import uniform, randint, shuffle
+    # from github_com.kennethreitz import requests
+    # assert requests.get('https://github.com/p-2022-091/modulos/network.py').status_code == 200
 
-#     # formating the log
-#     FORMAT = '%(asctime)-15s %(nodeip)s %(type)-8s, message: %(message)s'
-#     logging.basicConfig(format=FORMAT)
-#     d = {'nodeip': 'ip', 'type': 'Satellite-Container'}
-#     logger = logging.getLogger('tcpserver')
-#     logger.setLevel(1)
-# except Exception as ex:
-#     raise Exception(f"ERROR:\n{ex}")
-
-
-# if __name__ == '__main__':
-#     try:
-#         # Create the server, binding to ip HOST on PORT 
-#         with socketserver.ThreadingTCPServer((HOST, PORT), CustomTCPHandler) as server:
-#             logger.warning(f"Listening on {HOST} in port {PORT}", extra=d)
-#             # Activate the server; this will keep running until  interrupt the program with Ctrl-C
-#             server.serve_forever()
-
-#     except Exception as ex:
-#         raise Exception(f"main: {ex}")
-
-
-import socket
-from time import time
-from random import uniform
+    # formating the log
+    FORMAT = '%(asctime)-15s %(nodeip)s %(type)-8s, message: %(message)s'
+    logging.basicConfig(format=FORMAT)
+    d = {'nodeip': 'ip', 'type': 'Satellite-Container'}
+    logger = logging.getLogger('tcpserver')
+    logger.setLevel(1)
+except Exception as ex:
+    raise Exception(f"ERROR:\n{ex}")
 
 
 if __name__ == '__main__':
     try:
         HOST, PORT = "localhost", 9999
-        # SATELLITE_MIGRATION_TIME: float = uniform(100, 130)
-        data = bytes("dbm"+"7777777"+"1"*10, "utf-8")
-        print(f"type: {type(data)}, __sizeof__: {(data.__sizeof__())}")
-
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        # create_connection() -- connects to an address, with an optional timeout and
-        # with socket.create_connection(socket.AF_INET, socket.SOCK_STREAM, timeout=10.0) as sock:
+        NUM_OF_SATELLITE: int = 1000
+        SATELLITE_ID_LIST: list = [i for i in range(1000000000, 1000000000+NUM_OF_SATELLITE)]
+        shuffle(SATELLITE_ID_LIST)
+        SATELLITE_START_dBm: str = "4444dBm"
 
         while(True):
             SATELLITE_MIGRATION_TIME: float = uniform(100, 130)
+            idxSatelliteId: int = randint(0, NUM_OF_SATELLITE)
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 # Connect to server and send data
                 sock.settimeout(SATELLITE_MIGRATION_TIME)
-                # sock.setdefaulttimeout(3)
                 sock.connect((HOST, PORT))
                 start_time = time()
-                sock.sendall(bytes(data + "\n", "utf-8"))
-
+                sock.sendall(bytes(SATELLITE_START_dBm + SATELLITE_ID_LIST[idxSatelliteId] + "1"*100 + "\n", "utf-8"))
                 # Receive data from the server and shut dow
-                received = str(sock.recv(1024), "utf-8")
-                sock.sendall(bytes(data + "\n", "utf-8"))
-                print(f"sock.gettimeout(): {sock.gettimeout()}, time():{time()}, start_time: {start_time}, {time()-start_time}")
+                # received = str(sock.recv(1024), "utf-8")
+
                 while(sock.gettimeout() > time()-start_time):
                     pass
-
-            print(f"Sent:     {data}")
-            print(f"Received from the server:  {received}")
+            tempIdxSatelliteId: int = randint(0, NUM_OF_SATELLITE)
+            while(tempIdxSatelliteId == idxSatelliteId):
+                tempIdxSatelliteId: int = randint(0, NUM_OF_SATELLITE)
+            idxSatelliteId = tempIdxSatelliteId
     except Exception as ex:
         raise Exception(f"main: {ex}")
+
 
